dc7f176f37510c75232f4395c2a8f39bc1682e58
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2025-07-07 23:46:11 -0400

    nvim flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
-  description = "test config to update misterio77's starter config to 25.05";
+  description = "full os, vm, wip";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nvim.url = "github:paperweightt/.dotfiles/nvim"
+    nvim.url = "github:paperweightt/.dotfiles/nvim";
   };
 
   outputs = { self, nixpkgs, home-manager, ... } @ inputs: let
